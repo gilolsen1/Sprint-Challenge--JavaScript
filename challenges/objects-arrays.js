db6164566656ef1ce90b4f5d17d45d98dfc647d2
Gilbert Olsen <gilbertolsen@Gilberts-MacBook-Pro.local>
2019-04-06 12:32:24 -0400

    finished objects to line 56 dinosaur challenges

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -3,32 +3,57 @@
 /* 
   Given the following information about dinosaurs, create 3 objects: 
   Use this pattern to create your objects: 
-  object name, diet, weight, length, period
+  
+  object name, 
+  diet, 
+  weight, 
+  length, 
+  period
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
+const trex= {
+  name:"tyrannosaurus", 
+  diet:"carnivorous", 
+  weight:"7000kg", 
+  length:"12m", 
+  period:"Late Cretaceious",
+  roar () {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const  stego = {
+  name:"stegosaurus", 
+  diet:"herbivorous", 
+  weight:"2000kg", 
+  length:"9m", 
+  period:"Late Jurassic",
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-
+const raptor= {
+  name:"velociraptor", 
+  diet:"carnivorous", 
+  weight:"15kg", 
+  length:"12m", 
+  period:"Late Cretaceious",
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(`tyrannosaurus weighed ${trex.weight}`);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(`velociraptor was a hardcore paleo hipster his diet was ${raptor.diet}`);
 
 // How long was a stegosaurus?
-console.log();
+console.log(`teeth to tail spikes, stegosaurus was ${stego.length} long`);
 
 // What time period did tyrannosaurus live in?
-console.log();
-
+console.log(`if marty mcfly wants to mess with rex, set car to the ${trex.period} era`);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar());
 
 
 // ==== Arrays ====
